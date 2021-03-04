9d4a6bbcbd997e0a1355a5003b578f24aa65a8b3
dannyycc <dannychiu1612@gmail.com>
2021-03-04 11:42:50 +0000

    Added ethan's changes

--- chatfunctions.c
+++ chatfunctions.c
@@ -3,20 +3,23 @@
 
 void greeting()
 {
-	printf("hello\n");
+	printf("Hello, welcome to this amazing chat bot, written in C!\n");
+	printf("Type help to show the available commands\n");
 }
 
 void take_input(char * input)
-{
-	printf("Hello\n");
+{	
+	printf("chatbot ~ >> ");
+	strcpy(input, fgets(input,200,stdin)); //copy input from stdin to input pointer
 }
 
 void respond_to(char * input, char * response)
-{
-	printf("Hello\n");
+{	
+	puts(input); // just prints out what was inputted!
 }
 
 void goodbye()
-{
+{	
+	printf("Thanks for using this amazing chatbot :)");
 	printf("Au revoir!\n");
 }
